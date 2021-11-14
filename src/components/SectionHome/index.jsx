cae65020671a6c954c67e5e7e1ca4c96a7cdf7b6
huynham <91304084+huynham@users.noreply.github.com>
2021-11-14 15:23:19 +0700

    first commit

---
+++ src/components/SectionHome/index.jsx
@@ -0,0 +1,32 @@
+
+import './home.css'
+import das from '../../image/das.png';
+
+function Home(){
+    
+    return (
+        <section id="home">
+            <div className="home-sub">
+                <div className="home-sub-one">
+                    <div className="home-content-1">
+                        <h1>We help startups launch their products</h1>
+                        <p>Etiam sed.Interdum consequat proin vestibulum class at a euismod mus luctus quam.
+                            Lorem ipsum dolor sit amet, consectetur adipisicing eli.</p>
+                        <a href="#">Get Started <i class="fas fa-arrow-alt-right"></i></a>
+                          
+                    </div>
+                    <div className="home-content-2">
+                        <div className="home-content-2-sub">
+                            <img src={das} alt="nen" />
+                        </div>
+
+                    </div>
+                </div>
+                
+            </div>
+           
+            
+        </section>
+    )
+}
+export default Home
